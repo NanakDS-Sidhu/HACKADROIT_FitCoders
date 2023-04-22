547705b11b869c84bcab98a85cc54e4a6fa1eebc
anshRS <anshraj0426@gmail.com>
2023-04-22 22:31:44 +0530

    Fetched and Displayed the data for Card component from backend

--- pages/profile/[userId]/dashpost.js
+++ pages/profile/[userId]/dashpost.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../../components/Sidebar";
 import Card from "../../../components/Card";
 import Members from "@/components/Members";
+import supabase from "@/config/SupabaseConfig";
 const dashpost = () => {
+  const [eventData, setEventData] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+
+      let { data: Posts, error } = await supabase
+        .from('Posts')
+        .select('*')
+
+      if (error) {
+        console.log(error)
+      } else {
+        // console.log(Posts)
+        setEventData(Posts)
+      }
+    }
+    getPosts()
+  }, [])
   return (
     <>
       <div className="flex h-screen">
         <Sidebar />
         <div className="overflow-y-scroll">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            eventData && eventData.map((data)=>{
+              return(
+                <Card data={data}/>
+              )
+            })
+          }
         </div>
-        <Members/>
+        <Members />
       </div>
     </>
   );
