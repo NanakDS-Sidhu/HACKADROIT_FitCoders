179d431a5030f2509c3640ccf4eb0b534a935614
nanak <sidhu.nds22@gmail.com>
2023-04-22 22:05:09 +0530

    seperated chat into a component

--- components/Messages/YourMessage.js
+++ components/Messages/YourMessage.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function YourMessage(props) {
   return (
     <div>
+        <p className='flex justify-start mb-4'>Username</p>
         <div class="flex justify-start mb-4">
             <img
               src="https://images.unsplash.com/photo-1589254065909-b7086229d08c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
