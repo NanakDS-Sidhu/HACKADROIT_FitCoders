b6a82b61ce87ec5e47e619b3c5f469132ee67dff
anshRS <anshraj0426@gmail.com>
2023-04-22 12:26:15 +0530

    Modified the Layout

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -6,8 +7,9 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        <NextScript />
+        <NextScript />        
       </body>
+      <Script src="../path/to/flowbite/dist/flowbite.min.js" />      
     </Html>
   )
 }
