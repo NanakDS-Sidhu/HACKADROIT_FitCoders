7bacca8e2e24ed15989fe86b078b4ea9dc08188a
anshRS <anshraj0426@gmail.com>
2023-04-22 22:30:25 +0530

    Fetched and Displayed the Organization data from backend

--- components/OrgCard.js
+++ components/OrgCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const OrgCard = () => {
+const OrgCard = (props) => {
+  console.log(props)
   return (
     <>
       <figure class="flex flex-col  items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
         <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{props.data.Title}</h3>
             <p class="my-4">If you care for your time, I hands down would go with this."</p>
         </blockquote>
         <figcaption class="flex items-center justify-center space-x-3">
